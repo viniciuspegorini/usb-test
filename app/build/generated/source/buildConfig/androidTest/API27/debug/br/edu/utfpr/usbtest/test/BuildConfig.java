71f89362d0729d762df16c66d549ab3284adb50c
Vinicius Pegorini <viniciuspegorini@gmail.com>
2024-04-01 14:07:49 -0300

    first commit

---
+++ app/build/generated/source/buildConfig/androidTest/API27/debug/br/edu/utfpr/usbtest/test/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.edu.utfpr.usbtest.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.edu.utfpr.usbtest.api34.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "API27";
+}
---
+++ app/src/main/java/br/edu/utfpr/usbtest/MainActivity.kt
@@ -0,0 +1,293 @@
+package br.edu.utfpr.usbtest
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbConstants
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.os.Build
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+	
+	private lateinit var mainFrame: View
+	private lateinit var mLogScrollView: ScrollView
+	private lateinit var logTv: TextView
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		if (DEBUG) Log.v(TAG, "onCreate:")
+		setContentView(R.layout.activity_main)
+		this.title = "targetSDK${applicationInfo.targetSdkVersion}"
+		mainFrame = findViewById(R.id.main_frame)
+		mLogScrollView = findViewById(R.id.log_scrollview)
+		logTv = findViewById(R.id.log_textview)
+
+		// output information to screen
+		var msg = "targetSDKVersion=${applicationInfo.targetSdkVersion}"
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			msg = "${msg}\n    minSDKVersion=${applicationInfo.minSdkVersion}"
+		}
+		msg = "${msg}\n    SDK_INT=${Build.VERSION.SDK_INT}\n" +
+			"    BOARD=${Build.BOARD}\n" +
+			"    BOOTLOADER=${Build.BOOTLOADER}\n" +
+			"    BRAND=${Build.BRAND}\n" +
+			"    DEVICE=${Build.DEVICE}\n" +
+			"    DISPLAY=${Build.DISPLAY}\n" +
+			"    HARDWARE=${Build.HARDWARE}\n" +
+			"    ID=${Build.ID}\n" +
+			"    MANUFACTURER=${Build.MANUFACTURER}\n" +
+			"    PRODUCT=${Build.PRODUCT}\n" +
+			"    TAGS=${Build.TAGS}\n" +
+			"    VERSION.MODEL=${Build.MODEL}\n"
+		log("Info", msg)
+
+	}
+	
+	override fun onNewIntent(intent: Intent?) {
+		super.onNewIntent(intent)
+		if (DEBUG) Log.v(TAG, "onNewIntent:$intent")
+		if (intent != null) {
+			runOnUiThread {
+				handleIntent(intent)
+			}
+		}
+	}
+	
+	override fun onStart() {
+		super.onStart()
+		if (DEBUG) Log.v(TAG, "onStart:")
+		// Register BroadcastReceiver to receive USB related events
+		val filter = IntentFilter(ACTION_USB_PERMISSION)
+		filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+		filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
+		registerReceiver(mBroadcastReceiver, filter)
+		log("BroadcastReceiver", "register")
+		if (intent != null) {
+			runOnUiThread {
+				handleIntent(intent)
+			}
+		}
+	}
+	
+	override fun onStop() {
+		if (DEBUG) Log.v(TAG, "onStop:")
+		unregisterReceiver(mBroadcastReceiver)
+		log("BroadcastReceiver", "unregister")
+		super.onStop()
+	}
+	
+	override fun onDestroy() {
+		if (DEBUG) Log.v(TAG, "onDestroy:")
+		super.onDestroy()
+	}
+	
+//--------------------------------------------------------------------------------
+	/**
+	 * BroadcastReceiver to receive USB related events
+	 */
+	private val mBroadcastReceiver = object : BroadcastReceiver() {
+		override fun onReceive(context: Context?, intent: Intent?) {
+			if (intent != null) {
+				handleIntent(intent)
+			}
+		}
+	}
+	
+	/**
+	 * Handle received Intent
+	 */
+	private fun handleIntent(intent: Intent) {
+		if (DEBUG) Log.v(TAG, "handleIntent:$intent")
+		when (intent.action) {
+			UsbManager.ACTION_USB_DEVICE_ATTACHED
+				-> handleActionOnAttachDevice(intent)
+			UsbManager.ACTION_USB_DEVICE_DETACHED
+				-> handleActionOnDetachDevice(intent)
+			ACTION_USB_PERMISSION
+				-> handleActionUsbPermission(intent)
+			Intent.ACTION_MAIN
+				-> scanAttachedDevice()	// app launched by user
+			else -> {
+				log("Unknown intent", "action=" + intent.action)
+			}
+		}
+	}
+	
+	/**
+	 * when app received attach event of USB device
+	 */
+	private fun handleActionOnAttachDevice(intent: Intent) {
+		if (DEBUG) Log.v(TAG, "handleActionOnAttachDevice:$intent")
+		val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+		val device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+			} else {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+			}
+		var hasPermission = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
+
+		if (hasPermission) {
+			log("INFO VP", "hasPermission = true")
+		} else {
+			log("INFO VP", "hasPermission = false")
+		}
+
+		if (device != null) {
+			hasPermission = hasPermission || manager.hasPermission(device)
+			log("USB_DEVICE_ATTACHED", "hasPermission=$hasPermission\n    ${deviceName(device)}")
+
+			if (manager.hasPermission(device)) {
+				log("INFO VP", "managerHasPermission = true")
+			} else {
+				log("INFO VP", "managerHasPermission = false")
+			}
+
+			if (!hasPermission) {
+				log("INFO VP", "hasPermission = false - request permission")
+				requestUsbPermission(manager, device)
+			} else {
+				log("USB permission", "already has permission:\n    ${deviceName(device)}")
+			}
+		} else {
+			log("USB_DEVICE_ATTACHED", "device is null")
+		}
+	}
+	
+	/**
+	 * when app received detach event of USB device
+	 */
+	private fun handleActionOnDetachDevice(intent: Intent) {
+		if (DEBUG) Log.v(TAG, "handleActionOnDetachDevice:$intent")
+		val device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+			} else {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+			}
+		log("USB_DEVICE_DETACHED", deviceName(device))
+	}
+	
+	/**
+	 * when app received the result of requesting USB access permission.
+	 */
+	private fun handleActionUsbPermission(intent: Intent) {
+		if (DEBUG) Log.v(TAG, "handleActionUsbPermission:$intent")
+		val device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+			} else {
+				intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+			}
+		val hasPermission = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
+		log("Result", "hasPermission=$hasPermission\n    ${deviceName(device)}")
+	}
+
+//--------------------------------------------------------------------------------
+	/**
+	 * scan attached usb devices and request permission for first found device that has no permission
+	 */
+	private fun scanAttachedDevice() {
+		if (DEBUG) Log.v(TAG, "scanAttachedDevice:")
+		log("SCAN", "start")
+		val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+		val devices = manager.deviceList
+		run loop@ {
+			devices.values.forEach {
+				if (isUVC(it)) {
+					if (!manager.hasPermission(it)) {
+						requestUsbPermission(manager, it)
+						// XXX only request permission for first found device now
+						return@loop
+					} else {
+						log("USB permission", "already has permission:\n    ${deviceName(it)}")
+					}
+				}
+			}
+		}
+		log("SCAN", "finished")
+	}
+	
+	/**
+	 * request USB permission for specific device
+	 */
+	private fun requestUsbPermission(manager: UsbManager, device: UsbDevice) {
+		log("USB permission", "request\n    ${deviceName(device)}")
+		runOnUiThread {
+			var flags = 0
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+				flags = flags or PendingIntent.FLAG_MUTABLE
+			}
+			val permissionIntent = PendingIntent.getBroadcast(this, REQ_PERMISSION_USB,
+				 Intent(ACTION_USB_PERMISSION)
+				 	.setPackage(packageName),
+				flags)
+			manager.requestPermission(device, permissionIntent)
+		}
+	}
+	
+	/**
+	 * check whether the specific device or one of its interfaces is VIDEO class
+	 */
+	private fun isUVC(device: UsbDevice?): Boolean {
+		var result = false
+		if (device != null) {
+			if (device.deviceClass == UsbConstants.USB_CLASS_VIDEO) {
+				result = true
+			} else {
+				loop@ for (i in 0..device.interfaceCount) {
+					val iface = device.getInterface(i)
+					if (iface.interfaceClass == UsbConstants.USB_CLASS_VIDEO) {
+						result = true
+						break@loop
+					}
+				}
+			}
+		}
+		return result
+	}
+
+	/**
+	 * get device name of specific UsbDevice
+	 * return productName if it is available else return deviceName
+	 */
+	private fun deviceName(device: UsbDevice?): String {
+		var result = "device is null"
+		if (device != null) {
+			result = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				if (!TextUtils.isEmpty(device.productName)) device.productName!! else device.deviceName
+			} else {
+				device.deviceName
+			}
+		}
+		return result
+	}
+	
+//--------------------------------------------------------------------------------
+	/**
+	 * add message to TextView
+	 */
+	private fun log(tag: String, msg: String) {
+		runOnUiThread {
+			logTv.append("$tag:\n    $msg\n")
+			mLogScrollView.scrollTo(0, logTv.bottom)
+		}
+	}
+
+//--------------------------------------------------------------------------------
+	companion object {
+		private const val DEBUG = true
+		private val TAG = MainActivity::class.simpleName
+		private const val ACTION_USB_PERMISSION = "ACTION_USB_PERMISSION"
+		private const val REQ_PERMISSION_USB = 16
+		private const val REQ_PERMISSION_CAMERA = 18
+	}
+}
